3c82b37f991c5589ba2ca2481c792d7b76c4a75c
saadu ayo <me@saadudeenadebayo621.com>
2023-09-27 00:02:43 +0100

    first-commit

---
+++ src/MenuItems.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+const MenuItem = ({ img, title, price, desc }) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="menu-item">
+      <img src={img} alt={title} className="img" />
+      <div className="item-info">
+        <header>
+          <h5>{title}</h5>
+          <span className="item-price">${price}</span>
+        </header>
+        <p className="item-text">
+          {readMore ? desc : `${desc.substring(0, 200)}...`}
+
+          <button className="btn" onClick={() => setReadMore(!readMore)}>
+            {readMore ? "showless" : "readMore"}
+          </button>
+        </p>
+      </div>
+    </article>
+  );
+};
+export default MenuItem;
